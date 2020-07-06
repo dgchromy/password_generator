17f8e3f700c30e0bb6e3ba228243c3c3ca462ce6
Donald Chromy <dgchromy@gmail.comglobal user.emailgit config user.emailgit config --global user.email dgchromy@gmail.com>
2020-07-05 19:04:01 -0700

    updated java script and style.css

--- script1.js
+++ script1.js
@@ -1,12 +1,12 @@
 //DOM elements 
-const resultEl =document.getElementById("result");
-const lengthEl =document.getElementById("length");
-const uppercaseEl =document.getElementById("uppercase");
-const lowercaseEl =document.getElementById("lowercase");
-const numbersEl =document.getElementById("numbers");
-const symbolsEl =document.getElementById("symbols");
-const generateEl =document.getElementById("generate");
-const clipboardEl =document.getElementById("clipboard");
+const resultEl = document.getElementById("result");
+const lengthEl = document.getElementById("length");
+const uppercaseEl = document.getElementById("uppercase");
+const lowercaseEl = document.getElementById("lowercase");
+const numbersEl = document.getElementById("numbers");
+const symbolsEl = document.getElementById("symbols");
+const generateEl = document.getElementById("generate");
+const clipboardEl = document.getElementById("clipboard");
 
 
 const randomFunc = {
@@ -14,9 +14,22 @@ const randomFunc = {
     upper: getRandomUpper,
     number: getRandomNumber,
     symbol: getRandomSymbol,
-
-
 };
+
+clipboardEl.addEventListener("click", () => {
+    const textarea = document.createElement("textarea");
+    const password = resultEl.innerText;
+    if(!password){
+        return;
+    }
+    textarea.value = password;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand("copy");
+    textarea.remove();
+    alert("Copied to clipboard");
+});
+
 // generate event listen.//
 
 
@@ -25,31 +38,16 @@ generateEl.addEventListener("click", () => {
  const hasLower = lowercaseEl.checked;
  const hasUpper = uppercaseEl.checked;   
  const hasNumber = numbersEl.checked;   
- const hasSymbol = symbolEl.checked;      
+ const hasSymbol = symbolsEl.checked;      
  
  resultEl.innerText = generatePassword(
      hasLower,
-     hasNumber,
      hasUpper,
+     hasNumber,
      hasSymbol,
      length
      );
     
-});
-
-//clipboard copy function
-clipboardEl.addEventListener("click", () =>{
-    const textarea = document.createElement("textarea");
-    const password = resultEL.innerText
-    if(!password){
-        return;
-    }
-    textarea.value = password;
-    document.body.appendChild(textarea);
-    textarea.select();
-    document.execCommand("copy")
-    textarea.remove();
-    alert("Copied to clipboard");
 });
 
 // generate event listen.//
@@ -64,17 +62,17 @@ const typesArr = [ {lower}, {upper}, {number}, {symbol}].filter
 (
     item =>Object.values(item) [0]
 );
-if (typescount === 0) {
+if (typesCount === 0) {
     return "";
 }
 for(let i = 0; i< length; i += typesCount){
     typesArr.forEach(type =>{
         const funcName = Object.keys(type)[0];
 
-        generatedPassword += randomFunc[funcName]();
+        generatePassword += randomFunc[funcName]();
     });
  }
- const finalPassword = generatedPassword.slice(0, lenght);
+ const finalPassword = generatePassword.slice(0, length);
 
 return finalPassword;
 }
@@ -93,7 +91,7 @@ function getRandomUpper() {
 function getRandomNumber() { 
         return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
         }
- 
+
 function getRandomSymbol() {
      const symbols = "!@#$%^&*(){}+?=";
      return symbols[Math.floor(Math.random() * symbols.length)];
